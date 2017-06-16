ecb471942d974f9400bed0c122d544de0bbec492
Minero Aoki <minero-aoki@cookpad.com>
2017-06-16 12:29:18 +0900

    moves S3DataFileBundle and Logger to redshift-connector-data_file gem

--- test/test_reader.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-
-module RedshiftConnector
-  class TestReader < Test::Unit::TestCase
-    def test_get
-      assert_equal Reader::RedshiftCSV, Reader.get(:redshift_csv)
-    end
-  end
-end
