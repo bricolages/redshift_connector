5548711bc2ef35758e475f1074db225de59b9f55
Minero Aoki <minero-aoki@cookpad.com>
2019-03-07 14:03:18 +0900

    fix test for new version

--- test/reader/test_redshift_csv.rb
+++ test/reader/test_redshift_csv.rb
@@ -1,12 +1,12 @@
 require 'test/unit'
-require 'redshift-connector/data_file'
+require 'redshift_connector/data_file'
 
 module RedshiftConnector
   module Reader
     class TestRedshiftCSV < Test::Unit::TestCase
       def parse_row(line)
-        r = RedshiftCSV.new(nil)
-        r.parse_row(line, 1)
+        r = RedshiftCSV.new(StringIO.new(line))
+        r.read_row
       end
 
       def test_parse_row
@@ -24,6 +24,12 @@ module RedshiftConnector
 
         assert_equal ['981179', '2017-01-07', '6', '852', 'show', '{"page"=>"4"}', '1', '1'],
           parse_row(%Q("981179","2017-01-07","6","852","show","{\\"page\\"=>\\"4\\"}","1","1"\n))
+      end
+
+      def test_each_row
+        r = RedshiftCSV.new(StringIO.new(%Q("95"\n)))
+        assert_equal ['95'], r.read_row
+        assert_nil r.read_row
       end
     end
   end
